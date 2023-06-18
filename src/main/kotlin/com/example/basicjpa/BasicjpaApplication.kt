7b8a591e39096ac59d761b576637d0d61c904b4a
spark <pnw123456@icloud.com>
2023-06-18 11:32:32 +0900

    first commit

---
+++ src/main/kotlin/com/example/basicjpa/BasicjpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.basicjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BasicjpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<BasicjpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/basicjpa/BasicjpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.basicjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BasicjpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
